f9ed5e950c13694b652bd2d6ce7992755cebed85
gao.jingxiong <gao.jx@qq.com>
2022-05-08 17:29:34 +0800

    second commit

--- src/Demo.java
+++ src/Demo.java
@@ -6,5 +6,8 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
     }
 }
